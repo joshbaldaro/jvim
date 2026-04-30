1878718021ff4ad33e33e55eb5668aec665ff51d
Josh Baldaro <joshua@baldaro.me.uk>
2026-04-30 11:55:25 +0100

    added inline diagnostics

--- lua/core/options.lua
+++ lua/core/options.lua
@@ -16,3 +16,13 @@ opt.tabstop = 4
 opt.softtabstop = 4
 opt.expandtab = true
 
+vim.diagnostic.config({
+    virtual_text = {
+        prefix = "●",
+        spacing = 4,
+    },
+    signs = true,
+    underline = true,
+    update_in_insert = true,
+    severity_sort = true,
+})
